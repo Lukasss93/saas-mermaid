d06069c67ed12458eded2aed02c6baf75396dac3
Luca Patera <lucapatera@outlook.it>
2022-06-10 19:52:32 +0200

    Fix image size for jpg/png

--- src/Mermaider.ts
+++ src/Mermaider.ts
@@ -27,6 +27,47 @@ export default class Mermaider {
         return this;
     }
 
+    protected getChartType(): string | null {
+
+        if (this.text.match(/\b(graph|flowchart)\b/ig) !== null) {
+            return 'flowchart';
+        }
+
+        if (this.text.match(/\b(sequenceDiagram)\b/ig) !== null) {
+            return 'sequenceDiagram';
+        }
+
+        if (this.text.match(/\b(classDiagram)\b/ig) !== null) {
+            return 'classDiagram';
+        }
+
+        if (this.text.match(/\b(stateDiagram-v2)\b/ig) !== null) {
+            return 'stateDiagram-v2';
+        }
+
+        if (this.text.match(/\b(gantt)\b/ig) !== null) {
+            return 'gantt';
+        }
+
+        if (this.text.match(/\b(pie)\b/ig) !== null) {
+            return 'pie';
+        }
+
+        if (this.text.match(/\b(erDiagram)\b/ig) !== null) {
+            return 'erDiagram';
+        }
+
+        if (this.text.match(/\b(journey)\b/ig) !== null) {
+            return 'journey';
+        }
+
+        if (this.text.match(/\b(gitGraph)\b/ig) !== null) {
+            return 'gitGraph';
+        }
+
+        return null;
+    }
+
     protected async init(script, ...args) {
         let response: any = null;
         let browser = await puppeteer.launch({args: ["--no-sandbox"]});
@@ -42,15 +83,22 @@ export default class Mermaider {
             response = await renderPage.evaluate(script, ...args);
 
             //get canvas size
-            let regex = /<svg viewBox="(?<x>\d+) (?<y>\d+) (?<w>\d+\.?\d*) (?<h>\d+\.?\d*)"/;
+            let regex = /viewBox="(?<x>-?\d+\.?\d*) (?<y>-?\d+\.?\d*) (?<w>\d+\.?\d*) (?<h>\d+\.?\d*)"/;
             let match = regex.exec(response);
-            let width = Math.ceil(Number(match?.groups?.w || 0));
-            let height = Math.ceil(Number(match?.groups?.h || 0));
+            let w = Math.ceil(Number(match?.groups?.w || 0));
+            let h = Math.ceil(Number(match?.groups?.h || 0));
+            let offsetX = 0;
+            let offsetY = 0;
+
+            //get chart type
+            if (this.getChartType() === 'gitGraph') {
+                offsetX = 40;
+            }
 
             //convert svg if needed
             if (this.format !== 'svg') {
                 const conversionPage = await browser.newPage();
-                await conversionPage.setViewport({width: width, height: height});
+                await conversionPage.setViewport({width: w + offsetX, height: h + offsetY});
                 await conversionPage.setContent(response);
                 response = await conversionPage.screenshot({
                     ...(this.format !== 'png' ? {quality: 100} : {}),
